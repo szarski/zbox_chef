84f15ec150c176dc533117b336a712909b38fcdb
Jacek Szarski <jacek@applicake.com>
2012-05-13 16:45:28 +0200

    created initial chef-solo setup

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Usage: ./deploy.sh [host]
+
+host="${1:-chefclient@szarski.zapto.org}"
+
+# The host key might change when we instantiate a new VM, so
+# we remove (-R) the old host key from known_hosts
+ssh-keygen -R "${host#*@}" 2> /dev/null
+
+tar cj . | ssh -o 'StrictHostKeyChecking no' "$host" '
+sudo rm -rf /tmp/chef &&
+mkdir /tmp/chef &&
+cd /tmp/chef &&
+tar xj &&
+sudo bash install.sh'
