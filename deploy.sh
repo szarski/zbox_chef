bbce47954e0e6ab8dd81e698c04364f4b0b04b5b
Jacek Szarski <jacek.szarski@gmail.com>
2012-10-01 20:30:39 +0200

    made deploy read credentials from and external file

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Usage: ./deploy.sh [host]
+# Usage: ./deploy.sh
 
-host="${1:-chefclient@szarski.zapto.org}"
+host=`cat credentials`
 
 # The host key might change when we instantiate a new VM, so
 # we remove (-R) the old host key from known_hosts
